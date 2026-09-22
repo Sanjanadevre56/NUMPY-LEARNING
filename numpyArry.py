9d80a9ebd579ff0214457d3eaed1eab98d65f5c3
Sanjana Devre <devresanjana145@gmail.com>
2026-09-22 15:52:30 -0700

    numpay array

---
+++ numpyArry.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arry = np.array([1,2,3,4,5])
+print(type(arry))
+
+arry2=np.array([[5,6,7,8],[9,7,6,5]])
+print(type(arry2))
+print(arry2)
+
+# shape = (row ,col)
+print(arry2.shape)
